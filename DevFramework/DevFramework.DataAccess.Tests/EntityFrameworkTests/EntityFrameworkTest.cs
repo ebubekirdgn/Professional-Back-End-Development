b3768dee79e8720e6cbd78281083f3be01a4d2eb
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-05 22:32:45 +0300

    added Test

--- DevFramework/DevFramework.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTest.cs
+++ DevFramework/DevFramework.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTest.cs
@@ -15,5 +15,15 @@ namespace DevFramework.DataAccess.Tests.EntityFrameworkTests
 
             Assert.AreEqual(77,result.Count);
         }
+
+
+        [TestMethod]
+        public void Get_all_with_parameter_returns_filtered_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+            var result = productDal.GetList(p=> p.ProductName.Contains("ab"));
+
+            Assert.AreEqual(4, result.Count);
+        }
     }
 }

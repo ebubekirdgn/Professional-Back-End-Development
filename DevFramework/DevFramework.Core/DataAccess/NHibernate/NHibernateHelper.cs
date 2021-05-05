6c87eb8d9c356dbcff8f25c4b50d696f8b37fbce
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-05 12:32:08 +0300

    NHibernate created

---
+++ DevFramework/DevFramework.Core/DataAccess/NHibernate/NHibernateHelper.cs
@@ -0,0 +1,30 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+    public abstract class NHibernateHelper : IDisposable
+    {
+        static ISessionFactory _sessionFactor;
+
+        public virtual ISessionFactory SessionFactory
+        {
+            get { return _sessionFactor ?? (_sessionFactor = InitializeFactory()); }
+        }
+
+        protected abstract ISessionFactory InitializeFactory();
+
+        public virtual ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}

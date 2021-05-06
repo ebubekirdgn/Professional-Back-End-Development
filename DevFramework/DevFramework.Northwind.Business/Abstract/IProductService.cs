9987d549dc81f92bf3f05acc891ffcae89bd3910
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-06 22:41:05 +0300

    close #2

---
+++ DevFramework/DevFramework.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace DevFramework.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ DevFramework/DevFramework.Northwind.Business/Concrete/Managers/IProductManager.cs
@@ -0,0 +1,32 @@
+﻿using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace DevFramework.Northwind.Business.Concrete.Managers
+{
+    public class IProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public IProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+    }
+}

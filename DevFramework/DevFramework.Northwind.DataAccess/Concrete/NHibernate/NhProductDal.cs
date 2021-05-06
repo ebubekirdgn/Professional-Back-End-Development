a49ef059a530585abd5fbb1c462dfd71fed89f84
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-06 20:52:13 +0300

    NHibernate implementations

--- DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
+++ DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -2,19 +2,33 @@
 using DevFramework.Northwind.DataAccess.Abstract;
 using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
-using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NhProductDal : NhEntityRepositoryBase<Product>, IProductDal
     {
+        private NHibernateHelper _nhibernateHelper;
         public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
         }
         public List<ProductDetail> GetProductDetails()
         {
-            throw new NotImplementedException();
+            using (var session = _nhibernateHelper.OpenSession())
+            {
+                var result = from p in session.Query<Product>()
+                             join c in session.Query<Category>() on p.CategoryId equals c.CategoryId
+                             select new ProductDetail
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName
+                             };
+                return result.ToList();
+            }
+        
         }
+
     }
 }

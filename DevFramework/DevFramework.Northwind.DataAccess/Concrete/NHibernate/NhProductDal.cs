d0c050eaba3bfde910becf9c3ba63739c66af245
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-06 21:44:38 +0300

    update

--- DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
+++ DevFramework/DevFramework.Northwind.DataAccess/Concrete/NHibernate/NhProductDal.cs
@@ -2,21 +2,23 @@
 using DevFramework.Northwind.DataAccess.Abstract;
 using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
 {
     public class NhProductDal : NhEntityRepositoryBase<Product>, IProductDal
     {
         private NHibernateHelper _nhibernateHelper;
+
         public NhProductDal(NHibernateHelper nHibernateHelper) : base(nHibernateHelper)
         {
         }
+
         public List<ProductDetail> GetProductDetails()
         {
             using (var session = _nhibernateHelper.OpenSession())
-            { 
+            {
                 var result = from p in session.Query<Product>()
                              join c in session.Query<Category>() on p.CategoryId equals c.CategoryId
                              select new ProductDetail
@@ -27,8 +29,6 @@ namespace DevFramework.Northwind.DataAccess.Concrete.NHibernate
                              };
                 return result.ToList();
             }
-        
         }
-
     }
 }

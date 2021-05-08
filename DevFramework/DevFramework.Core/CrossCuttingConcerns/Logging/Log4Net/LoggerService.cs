c94392894b895b9b8a05016858c91fc33981c3c9
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-08 23:14:30 +0300

    LoggerService

---
+++ DevFramework/DevFramework.Core/CrossCuttingConcerns/Logging/Log4Net/LoggerService.cs
@@ -0,0 +1,58 @@
+﻿using log4net;
+using System;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    public class LoggerService
+    {
+        private ILog _log;
+
+        public LoggerService(ILog log)
+        {
+            _log = log;
+        }
+
+        public bool IsInfoEnabled => _log.IsInfoEnabled;
+        public bool IsDebugEnabled => _log.IsDebugEnabled;
+        public bool IsWarnEnabled => _log.IsWarnEnabled;
+        public bool IsFatalEnabled => _log.IsFatalEnabled;
+        public bool IsErrorEnabled => _log.IsErrorEnabled;
+
+        public void Info(Object logMessage)
+        {
+            if (IsInfoEnabled)
+            {
+                _log.Info(logMessage);
+            }
+        }
+
+        public void Debug(Object logMessage)
+        {
+            if (IsDebugEnabled)
+            {
+                _log.Debug(logMessage);
+            }
+        }
+        public void Warn(Object logMessage)
+        {
+            if (IsWarnEnabled)
+            {
+                _log.Warn(logMessage);
+            }
+        }
+        public void Fatal(Object logMessage)
+        {
+            if (IsFatalEnabled)
+            {
+                _log.Fatal(logMessage);
+            }
+        }
+        public void Error(Object logMessage)
+        {
+            if (IsErrorEnabled)
+            {
+                _log.Error(logMessage);
+            }
+        }
+    }
+}

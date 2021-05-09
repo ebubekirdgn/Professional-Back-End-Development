166a46c23d738d9dea1aa72199660450c750bef2
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-09 13:39:16 +0300

    LogAspect

--- DevFramework/DevFramework.Core/Aspects/PostSharp/LogAspects/LogAspect.cs
+++ DevFramework/DevFramework.Core/Aspects/PostSharp/LogAspects/LogAspect.cs
@@ -1,14 +1,16 @@
-﻿using DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net;
+﻿using DevFramework.Core.CrossCuttingConcerns.Logging;
+using DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net;
 using PostSharp.Aspects;
 using PostSharp.Extensibility;
 using System;
+using System.Linq;
 using System.Reflection;
 
 namespace DevFramework.Core.Aspects.PostSharp.LogAspects
 {
     [Serializable]
     //Bu kod sadece : nesne instanceslerinin örneklerinin methodlarına uygula hiçbir şekilde ctor'a uygulama
-    [MulticastAttributeUsage(MulticastTargets.Method, TargetMemberAttributes = = MulticastAttributes.Instance)]
+    [MulticastAttributeUsage(MulticastTargets.Method, TargetMemberAttributes = MulticastAttributes.Instance)]
     public class LogAspect : OnMethodBoundaryAspect
     {
         private Type _loggerType;
@@ -24,10 +26,37 @@ namespace DevFramework.Core.Aspects.PostSharp.LogAspects
             if (_loggerType.BaseType != typeof(LoggerService))
             {
                 throw new Exception("Wrong Logger Type");
-            } 
+            }
             _loggerService = (LoggerService)Activator.CreateInstance(_loggerType);
             base.RuntimeInitialize(method);
+        }
 
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            if (!_loggerService.IsInfoEnabled)
+            {
+                return;
+            }
+            try
+            {
+                var logParameters = args.Method.GetParameters().Select((t, i) => new LogParameter
+                {
+                    /*t = tip , i = iterator 0123 diye gidecek*/
+                    Name = t.Name,
+                    Type = t.ParameterType.Name,
+                    Value = args.Arguments.GetArgument(i)
+                }).ToList();
+                var logDetail = new LogDetail
+                {
+                    FullName = args.Method.DeclaringType == null ? null : args.Method.DeclaringType.Name,
+                    MethodName = args.Method.Name,
+                    Parameters = logParameters
+                };
+                _loggerService.Info(logDetail);
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }

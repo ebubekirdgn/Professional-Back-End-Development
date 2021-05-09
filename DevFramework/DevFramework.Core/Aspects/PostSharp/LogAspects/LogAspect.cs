6572484bbed99b17a007a89f576bc02dfc681551
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-09 15:00:33 +0300

    update

--- DevFramework/DevFramework.Core/Aspects/PostSharp/LogAspects/LogAspect.cs
+++ DevFramework/DevFramework.Core/Aspects/PostSharp/LogAspects/LogAspect.cs
@@ -15,12 +15,10 @@ namespace DevFramework.Core.Aspects.PostSharp.LogAspects
     {
         private Type _loggerType;
         private LoggerService _loggerService;
-
         public LogAspect(Type loggerType)
         {
             _loggerType = loggerType;
         }
-
         public override void RuntimeInitialize(MethodBase method)
         {
             if (_loggerType.BaseType != typeof(LoggerService))
@@ -30,7 +28,6 @@ namespace DevFramework.Core.Aspects.PostSharp.LogAspects
             _loggerService = (LoggerService)Activator.CreateInstance(_loggerType);
             base.RuntimeInitialize(method);
         }
-
         public override void OnEntry(MethodExecutionArgs args)
         {
             if (!_loggerService.IsInfoEnabled)

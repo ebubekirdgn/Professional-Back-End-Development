50abd2b298a9764ae26be61bebca27ee6c19654e
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-09 15:54:23 +0300

    ProductController and Model added

---
+++ DevFramework/DevFramework.Nortwind.MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using DevFramework.Nortwind.Business.Abstract;
+using DevFramework.Nortwind.Entities.Concrete;
+using DevFramework.Nortwind.MvcWebUI.Models;
+using System.Web.Mvc;
+
+namespace DevFramework.Nortwind.MvcWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var model = new ProductListViewModel
+            {
+                Products = _productService.GetAll()
+            };
+            return View(model);
+        }
+
+        public string Add()
+        {
+            _productService.Add(new Product
+            {
+                CategoryId = 1,
+                ProductName = "Gsm",
+                QuantityPerUnit = "1",
+                UnitPrice = 21
+            });
+            return "Added";
+        }
+
+        public string AddUpdate()
+        {
+            _productService.TransactionalOperation(new Product
+            {
+                CategoryId = 1,
+                ProductName = "Computer",
+                QuantityPerUnit = "1",
+                UnitPrice = 21
+
+            }, new Product
+            {
+                CategoryId = 1,
+                ProductName = "Computer 2",
+                QuantityPerUnit = "1",
+                UnitPrice = 30,
+                ProductId = 2
+            });
+            return "Done";
+        }
+    }
+}

bc14b1f6f313def4a39069a31fe2dc9dd3613292
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-12 22:23:10 +0300

    Update Global.asax

--- DevFramework/DevFramework.Nortwind.MvcWebUI/Global.asax.cs
+++ DevFramework/DevFramework.Nortwind.MvcWebUI/Global.asax.cs
@@ -1,8 +1,14 @@
+using DevFramework.Core.CrossCuttingConcerns.Security.Web;
 using DevFramework.Core.Utilities.Mvc.Infrastructure;
 using DevFramework.Northwind.Business.DependencyResolves.Ninject;
 using DevFramework.Nortwind.MvcWebUI;
+using System;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace DevFramework.Northwind.MvcWebUI
 {
@@ -13,6 +19,42 @@ namespace DevFramework.Northwind.MvcWebUI
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(new BusinessModule()));
+        }
+
+        public override void Init()
+        {
+            PostAuthenticateRequest += MvcApplication_PostAuthenticateRequest;
+            base.Init();
+        }
+
+        private void MvcApplication_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (authCookie == null)
+                {
+                    return;
+                }
+
+                var encTicket = authCookie.Value;
+                if (String.IsNullOrEmpty(encTicket))
+                {
+                    return;
+                }
+
+                var ticket = FormsAuthentication.Decrypt(encTicket);
+
+                var securityUtlities = new SecurityUtilities();
+                var identity = securityUtlities.FormAuthTicketToIdentity(ticket);
+                var principal = new GenericPrincipal(identity, identity.Roles);
+
+                HttpContext.Current.User = principal;
+                Thread.CurrentPrincipal = principal;
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }

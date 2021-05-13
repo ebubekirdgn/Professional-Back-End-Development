40476087ac3f1c91a4bdd4ace93aebe3c365f0f1
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-13 23:39:16 +0300

    close #7

--- DevFramework/DevFramework.Northwind.Entities/Concrete/UserRole.cs
+++ DevFramework/DevFramework.Northwind.Entities/Concrete/UserRole.cs
@@ -5,7 +5,7 @@ namespace DevFramework.Northwind.Entities.Concrete
 {
     public class UserRole : IEntity
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public int RoleId { get; set; }
         public int UserId { get; set; }
 

fe154474016fb1344781708e08a8757624331b74
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-16 22:47:34 +0300

    InstanceFactory

---
+++ MemberRegistration/MemberRegistration.Business/DependecyResolver/InstanceFactory.cs
@@ -0,0 +1,14 @@
+﻿using MemberRegistration.Business.DependecyResolver.Ninject;
+using Ninject;
+
+namespace MemberRegistration.Business.DependecyResolver
+{
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var kernel = new StandardKernel(new BusinessModule());
+            return kernel.Get<T>();
+        }
+    }
+}

85e4ebb12d1144f594d6e548c2c26dd42b045bd3
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-16 16:59:15 +0300

    Member Entities Created

---
+++ MemberRegistration/MemberRegistration.Entities/Concrete/Member.cs
@@ -0,0 +1,15 @@
+﻿using DevFramework.Core.Entities;
+using System;
+
+namespace MemberRegistration.Entities.Concrete
+{
+    public class Member : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TcIdentityNo { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}

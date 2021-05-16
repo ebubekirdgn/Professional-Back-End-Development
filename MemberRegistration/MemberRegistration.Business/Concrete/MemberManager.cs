ef388bd42c16cae03eabf726d45fa2e973609247
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-16 19:09:14 +0300

    KpsService added

---
+++ MemberRegistration/MemberRegistration.Business/Concrete/MemberManager.cs
@@ -0,0 +1,25 @@
+﻿using MemberRegistration.Business.Abstract;
+using MemberRegistration.Business.ServiceAdapters;
+using MemberRegistration.DataAccess.Abstract;
+using MemberRegistration.Entities.Concrete;
+
+namespace MemberRegistration.Business.Concrete
+{
+    public class MemberManager : IMemberService
+    {
+        private IMemberDal _memberDal;
+        private IKpsService _kpsService;
+        //Dependect Injection yaptık.
+        public MemberManager(IMemberDal memberDal, IKpsService kpsService)
+        {
+            _memberDal = memberDal;
+            _kpsService = kpsService;
+        }
+
+        public void Add(Member member)
+        {
+
+            _memberDal.Add(member);
+        }
+    }
+}
---
+++ MemberRegistration/MemberRegistration.Business/ServiceAdapters/KpsServiceAdapter.cs
@@ -0,0 +1,15 @@
+﻿using MemberRegistration.Business.ServiceAdapters;
+using MemberRegistration.Entities.Concrete;
+using System;
+
+namespace MemberRegistration.Business.KpsServiceReference
+{
+    public class KpsServiceAdapter : IKpsService
+    {
+        public bool ValidateUser(Member member)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrula(Convert.ToInt64(member.TcIdentityNo), member.FirstName.ToUpper(), member.LastName.ToUpper(), member.DateOfBirth.Year);
+        }
+    }
+}

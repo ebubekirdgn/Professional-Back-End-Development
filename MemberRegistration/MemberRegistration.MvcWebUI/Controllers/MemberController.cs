082b44887a2cf6c44c1d7d9cdd1266e07f74c8e6
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-18 21:34:49 +0300

    user add

--- MemberRegistration/MemberRegistration.MvcWebUI/Controllers/MemberController.cs
+++ MemberRegistration/MemberRegistration.MvcWebUI/Controllers/MemberController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using MemberRegistration.Entities.Concrete;
+using MemberRegistration.MvcWebUI.Models;
 using System.Web.Mvc;
 
 namespace MemberRegistration.MvcWebUI.Controllers
@@ -11,7 +9,13 @@ namespace MemberRegistration.MvcWebUI.Controllers
         // GET: Member
         public ActionResult Add()
         {
-            return View();
+            return View(new MemberAddViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Add(Member member)
+        {
+            return View(new MemberAddViewModel());
         }
     }
 }
